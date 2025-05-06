69da0f4c0ad3016497788d6d3dd138284e27fede
os-dev <oswaldo32gonzalez@gmail.com>
2025-05-06 11:15:53 -0600

    Start project, create responsive Header "it does not have funcionality yet"

---
+++ 02-portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,64 @@
+import styles from "./Header.module.css";
+import { useState } from "react";
+
+export default function Header() {
+  const [active, setActive] = useState(false);
+
+  function toggleMenu() {
+    setActive(active ? false : true);
+  }
+
+  return (
+    <header className={styles.header}>
+      <section className={styles.titleSection}>
+        <h1 className={styles.longTitle}>Oswaldo Gonzalez</h1>
+        <h1 className={styles.shortTitle}>OG</h1>
+      </section>
+      <section className={styles.menuSection}>
+        <nav>
+          <ul className={active ? styles.menuList : styles.hiddenMenuList}>
+            <li className={styles.menuItem}>Home</li>
+            <li className={styles.menuItem}>About</li>
+            <li className={styles.menuItem}>Projects</li>
+            <li className={styles.menuItem}>Contact</li>
+          </ul>
+        </nav>
+        <div className={styles.menuButtonSection}>
+          <button className={styles.menuButton} onClick={toggleMenu}>
+            {active ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={styles.menuSVG}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className={styles.menuSVG}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+      </section>
+    </header>
+  );
+}
