53723a3df9514024fa3122a8fc19ba80a8124757
os-dev <oswaldo32gonzalez@gmail.com>
2025-05-06 14:07:21 -0600

    Limit width on Header

--- 02-portfolio/src/components/Header/Header.jsx
+++ 02-portfolio/src/components/Header/Header.jsx
@@ -11,73 +11,75 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
-      <section className={styles.titleSection}>
-        <Link to="/" className={styles.titleSection}>
-          <h1 className={styles.longTitle}>Oswaldo Gonzalez</h1>
-          <h1 className={styles.shortTitle}>OG</h1>
-        </Link>
-      </section>
-      <section className={styles.menuSection}>
-        <nav className={styles.navSection}>
-          <ul className={active ? styles.menuList : styles.hiddenMenuList}>
-            <li className={styles.menuItem} onClick={toggleMenu}>
-              <Link className={styles.link} to="/">
-                Home
-              </Link>
-            </li>
-            <li className={styles.menuItem} onClick={toggleMenu}>
-              <Link className={styles.link} to="/about">
-                About
-              </Link>
-            </li>
-            <li className={styles.menuItem} onClick={toggleMenu}>
-              <Link className={styles.link} to="/projects">
-                Projects
-              </Link>
-            </li>
-            <li className={styles.menuItem} onClick={toggleMenu}>
-              <Link className={styles.link} to="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className={styles.menuButtonSection}>
-          <button className={styles.menuButton} onClick={toggleMenu}>
-            {active ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className={styles.menuSVG}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18 18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className={styles.menuSVG}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-            )}
-          </button>
-        </div>
-      </section>
+      <div className={styles.container}>
+        <section className={styles.titleSection}>
+          <Link to="/" className={styles.titleSection}>
+            <h1 className={styles.longTitle}>Oswaldo Gonzalez</h1>
+            <h1 className={styles.shortTitle}>OG</h1>
+          </Link>
+        </section>
+        <section className={styles.menuSection}>
+          <nav className={styles.navSection}>
+            <ul className={active ? styles.menuList : styles.hiddenMenuList}>
+              <li className={styles.menuItem} onClick={toggleMenu}>
+                <Link className={styles.link} to="/">
+                  Home
+                </Link>
+              </li>
+              <li className={styles.menuItem} onClick={toggleMenu}>
+                <Link className={styles.link} to="/about">
+                  About
+                </Link>
+              </li>
+              <li className={styles.menuItem} onClick={toggleMenu}>
+                <Link className={styles.link} to="/projects">
+                  Projects
+                </Link>
+              </li>
+              <li className={styles.menuItem} onClick={toggleMenu}>
+                <Link className={styles.link} to="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.menuButtonSection}>
+            <button className={styles.menuButton} onClick={toggleMenu}>
+              {active ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={styles.menuSVG}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className={styles.menuSVG}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </section>
+      </div>
     </header>
   );
 }
